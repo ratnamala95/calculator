820c1ea3e336f4555e60b552e8164359b6fe3ff7
gourav_agarwal123 <gourav@justaddwater.in>
2019-06-24 15:39:37 +0530

    resolved!

--- js/calc.js
+++ js/calc.js
@@ -1,74 +1,75 @@
 var queue = [];
 var input = 0;
+var result = 0;
 
 function clearAll(){
-  var queue = [];
-  var input = 0;
-  document.getElementById("answer").innerHTML = "0";
+    queue = [];
+    input = 0;
+    document.getElementById("answer").innerHTML = "0";
 }
 
 function calculation(value){
-  console.log(queue,input);
-  if (input!==0) {
+    if (input !==0) {
+      // console.log(input);
     input = parseFloat(input);
-    addToQueue(input);
-    input = 0;
-  }
-  var answer = value[0];
-  var dividedByZero = 0;
 
-  for (var i = 2; i < value.length; i+=2) {
-    switch (queue[i-1]) {
-      case '+':
-          answer+=value[i];
-          break;
-      case '-':
-          answer-=value[i];
-          break;
-      case '/':if(value[i]===0)  dividedByZero = 1;
-          else answer = answer / value[i];
-          break;
-      case '*':
-          answer= answer * value[i];
-        break;
+        addToQueue(input);
+    }
+    var answer = value[0];
+    var dividedByZero = 0;
+    for (  var i = 2; i < value.length; i= i+2) {
+
+        switch (queue[i-1]) {
+            case '+':
+                answer+= value[i];
+                break;
+            case '-':
+                answer-= value[i];
+                break;
+            case '/':    if (value[i] === 0)   dividedByZero = 1;
+                    else      answer = answer / value[i];
+
+                break;
+            case'*': answer = answer * value[i];
+                break;
+        }
 
     }
-  }
 
+    answer = answer.toFixed(2);
+    answer = parseFloat(answer);
+    if ( dividedByZero === 1) { clearAll();
+        document.getElementById("answer").innerHTML =  "ERROR";}
+    else
+    {document.getElementById("answer").innerHTML =  answer ;
+        input = answer;
+    queue = [];}
 
-
-  answer = answer.toFixed(2);
-  // answer = parseFloat(answer);
-
-  if (dividedByZero === 1){
-    clearAll();
-    document.getElementById("answer").innerHTML = "ERROR";
-  }else {
-    document.getElementById("answer").innerHTML = answer;
-    input = answer;
-    queue = [];
-  }
 
 }
 
 function numberButton(arg){
 
-  if(document.getElementById("answer").innerHTML === "ERROR" || document.getElementById("answer").innerHTML == "0"){
+  if(document.getElementById("answer").innerHTML === "ERROR" || (document.getElementById("answer").innerHTML == "0" && arg != ".")){
     document.getElementById("answer").innerHTML = "";
   }
   input += arg;
-  document.getElementById("answer").innerHTML += arg;
+  document.getElementById("answer").innerHTML += arg;/*display number*/
 
 }
 
 function operatorButton(arg){
-  if (input!=0){
+  if (input!==0 && input !== "-"){
     input = parseFloat(input);
     addToQueue(input);
     addToQueue(arg);
     document.getElementById("answer").innerHTML += arg;/* display operator*/
+    input = 0;
   }
-  input = 0;
+  if (arg == "-"  && isNaN(queue[0]) && input !== "-"){
+    input ="-";
+    document.getElementById("answer").innerHTML = "-";
+  }
   // alert(input);
 }
 

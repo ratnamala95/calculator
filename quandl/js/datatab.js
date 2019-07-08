e1c3a8489098b8ce4c3681d301b03aab7d945451
ratnamala <yadav.ratnamala95@gmail.com>
2019-07-08 15:25:32 +0530

    resolved..!! /*return manupulated data!!*/

--- quandl/js/datatab.js
+++ quandl/js/datatab.js
@@ -5,30 +5,16 @@ $(document).ready(() => {
       url:'https://www.quandl.com/api/v3/datasets/NASDAQOMX/NQUSB8775LMT.json?api_key=ksxyQgkKkcpYRiKkXVKZ',
       type:'GET',
       dataSrc:function(response){
-        var datum = JSON.stringify(response.dataset.data);
-        console.log(datum);
+        var $datum = {
+          data:[
+          ]
+        }
+        $arr = response.dataset.data;
+
+        $datum.data = $arr;
+        return $datum.data;
+        console.log($datum);
       }
     }
   });
-  // toJson();
 });
-
-function toJson(){
-  $.ajax({
-    url:'https://www.quandl.com/api/v3/datasets/NASDAQOMX/NQUSB8775LMT.json?api_key=ksxyQgkKkcpYRiKkXVKZ',
-    type:'GET',
-    success:function(response){
-      var datum = response.dataset.data;
-      var $arr = {
-        data:[]
-      };
-
-      for (var i = 0; i < 2; i++) {
-        $arr.data.push(datum[i]);
-        // alert(datum[i]);
-      }
-      $arr  = JSON.stringify($arr);
-      console.log($arr);
-    }
-  });
-}

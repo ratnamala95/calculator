10216c04757f3e94f5c105cd3a5ef5c019ef66b9
ratnamala <yadav.ratnamala95@gmail.com>
2019-07-08 14:35:28 +0530

    error persists!!

--- quandl/js/datatab.js
+++ quandl/js/datatab.js
@@ -4,6 +4,41 @@ $(document).ready(() => {
     ajax:{
       url:'https://www.quandl.com/api/v3/datasets/NASDAQOMX/NQUSB8775LMT.json?api_key=ksxyQgkKkcpYRiKkXVKZ',
       type:'GET',
+      dataSrc:function(response){
+        var datum = response.dataset.data;
+        var $arr = {
+          data:[]
+        };
+        for (var i = 0; i < 10; i++) {
+          $arr.data.push(datum[i]);
+          // alert(datum[i]);
+        }
+        console.log($arr);
+      }
     }
   });
+  // toJson();
 });
+
+function toJson(){
+  $.ajax({
+    url:'https://www.quandl.com/api/v3/datasets/NASDAQOMX/NQUSB8775LMT.json?api_key=ksxyQgkKkcpYRiKkXVKZ',
+    type:'GET',
+    success:function(response){
+      var datum = response.dataset.data;
+      var $arr = {
+        data:[]
+      };
+      // var $data = {
+      //   data :[
+      //   ]
+      // };
+
+      for (var i = 0; i < 2; i++) {
+        $arr.data.push(datum[i]);
+        // alert(datum[i]);
+      }
+      console.log($arr);
+    }
+  });
+}

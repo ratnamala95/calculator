ae5d3661f177b5841fab170ed20ffc455755526a
ratnamala <yadav.ratnamala95@gmail.com>
2019-07-08 16:42:52 +0530

    changes changes!!

--- quandl/js/datatab.js
+++ quandl/js/datatab.js
@@ -1,20 +1,14 @@
 $(document).ready(() => {
   $("#datatab").DataTable({
-    serverSide:true,
-    ajax:{
+    "serverSide":true,
+    "ajax":{
       url:'https://www.quandl.com/api/v3/datasets/NASDAQOMX/NQUSB8775LMT.json?api_key=ksxyQgkKkcpYRiKkXVKZ',
       type:'GET',
       dataSrc:function(response){
-        var $datum = {
-          data:[
-          ]
-        }
-        $arr = response.dataset.data;
-
-        $datum.data = $arr;
-        return $datum.data;
-        console.log($datum);
-      }
+        return response.dataset.data;
+      },
+      "processing":true,
+      "paging":true
     }
   });
 });
